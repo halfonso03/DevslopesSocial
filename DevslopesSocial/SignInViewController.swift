91e6581bff0cdb01e418559e941cdc2c5b34d034
halfonso03 <hector.alfonso@yahoo.com>
2016-12-19 15:48:03 -0500

    second facebook infrastructure

--- DevslopesSocial/SignInViewController.swift
+++ DevslopesSocial/SignInViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKLoginKit
+
 
 class SignInViewController: UIViewController {
 
@@ -20,6 +23,34 @@ class SignInViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func faceBookButtonPressed(_ sender: UIButton) {
+        
+        let faceBookLogin = FBSDKLoginManager()
+        
+        faceBookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print ("HECTOR: Error authenticating with facebook \(error)")
+            } else if (result?.isCancelled)! {
+                print ("HECTOR: user canlled fb auth")
+            } else {
+                print ("HECTOR: Authed with fb")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential ) {
+        
+        FIRAuth.auth()?.signIn(with: credential) { (user, error) in
+            if error != nil {
+                print ("HECTOR: unable to auth with firebase")
+            }
+            else {
+                print ("HECTOR: Authed with Firebase")
+            }
+        }
+    }
 
 }
 
